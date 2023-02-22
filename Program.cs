d6e53c4ded598066b16230dc909443dbd775d8f9
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2023-02-22 23:19:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            int number;
+            for ( number = 500; number < 600; number++)
+            {
+                int counter = 0;
+                for (int j = 2; j <= number / 2; j++)
+                {
+                    if (number % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+
+                if (counter == 0 && number != 1)
+                {
+                    Console.Write("{0} ", number);
+                }
+            }
+            Console.ReadKey();
+        }
+	}
+}
